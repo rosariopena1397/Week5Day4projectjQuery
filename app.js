b3a3a17b3f7b0c0f2dec81a953fd06dae5e49d12
rosariopena1397 <133817865+rosariopena1397@users.noreply.github.com>
2023-07-11 18:01:27 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+$(".navItem").each(function() {
+    $(this).css('color', 'white');
+   });
+
+$('#nav').css('background-color', '#145000');
+
+$('li:even').css("background-color", '#ccc');
+   
+
+$('li:odd').css("background-color", '#aaa');
+$('#myInput').val("Rosie");
+    
